d5268fcd13a29f41bfa1534b68bf44eb4c27bdbd
LeX?! <fatal.trace@gmail.com>
2021-09-06 19:19:53 +0000

    feat: Inited project.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,45 @@
 use std::env;
-use std::io;
+use std::io::{
+    self,
+    BufReader,
+    Read,
+    Write
+};
+use std::fs::File;
 use std::collections::HashMap;
 use walkdir::{ DirEntry, WalkDir };
+use data_encoding::HEXUPPER;
+use ring::digest::{Context, Digest, SHA512};
 
-struct File {
-    path: String
+fn sha512_digest<R: Read>(mut reader: R) -> io::Result<Digest> {
+    let mut context = Context::new(&SHA512);
+    let mut buffer = [0; 1024];
+
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        context.update(&buffer[..count]);
+    }
+
+    Ok(context.finish())
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let folder = args.get(1).unwrap();
-    let nested_files = WalkDir::new(folder)
-        .into_iter()
-        .filter_entry(|entry| entry.metadata().unwrap().is_file());
+    
+    for file in WalkDir::new(folder)
+                        .into_iter()
+                        .filter_map(|e| e.ok())
+                        .filter(|entry| entry.metadata().unwrap().is_file()) {
+        let input = File::open(file.path()).unwrap();
+        let reader = BufReader::new(input);
+        let digest = sha512_digest(reader).unwrap();
+                        
+        println!("SHA-512 digest is {}", HEXUPPER.encode(digest.as_ref()));
+    }
 
     let mut hashes: HashMap<&str, File> = HashMap::new();
 }
